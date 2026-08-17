101fc9321b2b0f9567a72448194a4bb15863fe9c
Arwa37 <arwaalzagri@gmail.com>
2026-08-17 09:20:50 +0300

    Code fixed

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1cepViZFOzLF2rU-gi-IScg4GN_ZtOM1t
 """
 
-! pip install streamlit
 
 import streamlit as st
 from transformers import pipeline
